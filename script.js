f2952da2f4501532addb066f2cac5f54405884fd
EternalFlyn <EternalFlyn@gmail.com>
2020-05-05 01:34:24 +0800

    1.0.2.5d

--- script.js
+++ script.js
@@ -1,79 +1,29 @@
-var w, h;
-var context;
+$(document).ready(function(){
+	$(".menuButton ul").hide();
+	$("#menuIcon").click(function() {
+		$("#menuContent").slideDown();
+	})
+	$("#menu").mouseleave(function() {
+		$("#menuContent").fadeOut();
+	})
+	$(".menuTitle").click(function() {
+		$(this).parent().children("ul").toggle();
+	})
+	$(".menuButton li").click(function() {
+		jump($(this).parent().parent().children(".menuTitle").text(), $(this).text());
+	})
+});
 
-function load() {
-	window.onresize = function() {
-		draw();
+// $(function() {
+// 	$.get("https://spreadsheets.google.com/feeds/list/1wlPcHLWdDB6fzMWJv2do26qkJH26QyUj7rF2yx-_5UI/1/public/values?alt=json", function(data) {
+// 		console.log(data);
+// 	})
+// })
+
+function jump(title, id) {
+	if(title == $("#title").text()) {
+		let pos = $("#" + id).offset().top - $("#header").height();
+		$("html,body").animate({scrollTop:pos}, 800);
 	}
-	draw();
-}
-
-function draw() {
-	let div = document.getElementsByClassName("content")[0];
-	w = div.clientWidth;
-	h = div.clientHeight;
-	var head = document.getElementById('magic');
-	if(head) {
-		context = head.getContext("2d");
-
-		head.width = w;
-		head.height = h;
-
-		context.fillStyle = "#000";
-		context.fillRect(0, 0, window.innerWidth, window.innerHeight);
-		magic(0.5, 0.5, "黑より黑く 闇より暗き漆黑に 我が深紅の混淆を望みたもう 覺醒のとき來たれり 無謬の境界に落ちし理 無行の歪みとなりて 現出せよ！ Explosion!", "#F00", 15, 20);
-		magic(0.5, 0.5, "test", "#0F0", 2, 10);
-	}
-}
-
-function getW(i) {
-	return w * i;
-}
-
-function getH(i) {
-	return h * i;
-}
-
-function strokeCircle(xp, yp, radius) {
-	context.beginPath();
-	context.arc(getW(xp), getH(yp), radius, 0, 2 * Math.PI);
-	context.stroke();
-}
-
-function fillCircle(xp, yp, radius) {
-	let x = window.innerWidth * xp, y = window.innerHeight * yp;
-	context.beginPath();
-	context.arc(getW(xp), getH(yp), radius, 0, 2 * Math.PI);
-	context.fill();
-}
-
-function circleRainbowColor(x1p, y1p, x2p, y2p, r) {
-	let x1 = getW(x1p), x2 = getW(x2p), y1 = getH(y1p), y2 = getH(y2p);
-	let color = context.createRadialGradient(x1, y1, 0, x2, y2, r);
-	color.addColorStop(0, "red");
-	color.addColorStop(0.167, "orange");
-	color.addColorStop(0.333, "yellow");
-	color.addColorStop(0.5, "green");
-	color.addColorStop(0.667, "blue");
-	color.addColorStop(0.833, "indigo");
-	color.addColorStop(1, "purple");
-	return(color)
-}
-
-function magic(xp, yp, text, color, size, fontSize) {
-	let base = fontSize * size;
-	context.save();
-	context.strokeStyle = color;
-	strokeCircle(xp, yp, base - fontSize * 0.3);
-	strokeCircle(xp, yp, base + fontSize);
-
-	context.fillStyle = color;
-	context.font = "bold " + fontSize + "px Arial";
-
-	context.translate(getW(xp), getH(yp));
-	for(let i = 0; i < text.length; i++) {
-		context.fillText(text[i], -size, -base);
-		context.rotate(2 * Math.PI / text.length);
-	}
-	context.restore();
+	console.log(title);
 }
