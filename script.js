007c708cc15fce0335bf4e84013ef5f4039a70b0
EternalFlyn <EternalFlyn@gmail.com>
2020-05-05 17:01:55 +0800

    1.0.2.5f

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-$.ajax({
-	type: "GET",
-	url: "https://eternalflyn.github.io/data.json",
-	dataType: "json",
-	success: function(data) {
-		console.log(data);
-	},
-	fail: function() {
-		console.log("GG")
-	}
-});
-
 $(document).ready(function(){
-	$(".menuButton ul").hide();
 	$("#menuIcon").click(function() {
 		$("#menuContent").slideDown();
 	})
 	$("#menu").mouseleave(function() {
 		$("#menuContent").fadeOut();
 	})
-	$(".menuTitle").click(function() {
-		$(this).parent().children("ul").toggle();
-	})
-	$(".menuButton li").click(function() {
-		jump($(this).parent().parent().children(".menuTitle").text(), $(this).text());
-	})
+	$.ajax({
+		type: "GET",
+		url: "https://eternalflyn.github.io/data.json",
+		dataType: "json",
+		success: function(data) {
+			addButton(data.menu);
+		}
+	});
 });
 
 // $(function() {
@@ -32,10 +21,45 @@ $(document).ready(function(){
 // 	})
 // })
 
-function jump(title, id) {
+function jump(title, id, link) {
 	if(title == $("#title").text()) {
 		let pos = $("#" + id).offset().top - $("#header").height();
 		$("html,body").animate({scrollTop:pos}, 800);
 	}
+	else {
+		$(location).attr("href", link);
+	}
 	console.log(title);
+}
+
+function addButton(menu) {
+	$.each(menu, function(i, n) {
+		let title = n["title"];
+		let item = n["item"];
+		let link = n ["link"];
+
+		let button = document.createElement("div");
+		let titleDiv = document.createElement("div");
+		let ul = document.createElement("ul");
+
+		button.className = "menuButton";
+		button.appendChild(titleDiv);
+		button.appendChild(ul);
+		
+		titleDiv.className = "menuTitle";
+		titleDiv.innerHTML = title;
+		
+		ul.className = title;
+		for(let j = 0; j < item.length; j++) {
+			let li = document.createElement("li");
+			li.innerHTML = item[j];
+			li.onclick = function() {jump(title, item[j], link);};
+			ul.appendChild(li);
+		}
+
+		$("#menuContent").append(button);
+	})
+	$(".menuTitle").click(function() {
+		$(this).parent().children("ul").toggle();
+	})
 }
